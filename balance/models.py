005a2f83ba97f0b18d308899253dbe60c6b5dfad
Kenan <kenan.mcgrath@gmail.com>
2016-05-15 23:21:34 +0100

    Make date field unique and indexed

--- balance/models.py
+++ balance/models.py
@@ -12,7 +12,7 @@ from balance.database import Base
 class Balance(Base):
     __tablename__ = 'balance'
     id = Column(Integer, primary_key=True)
-    date = Column(Date, nullable=False)
+    date = Column(Date, unique=True, index=True, nullable=False)
     amount = Column(Float, nullable=False)
 
     def __init__(self, amount, date):
