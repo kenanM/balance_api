5d43344eca1a7010e47e3a6b645e01d6dec53210
Kenan <kenan.mcgrath@gmail.com>
2016-05-16 22:33:03 +0100

    Get the database from an environment variable

--- balance/database.py
+++ balance/database.py
@@ -1,9 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(
-    'sqlite:////home/kenan/workspace/balance_api/balance.db', convert_unicode=True)
+engine = create_engine(os.environ['BALANCE_DATABASE'], convert_unicode=True)
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
